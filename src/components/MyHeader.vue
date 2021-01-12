6e09a8236a8b87f8e1ec55db51bb8c4217a5667a
Odeh Adejoh <42589643+stradox4u@users.noreply.github.com>
2021-01-12 22:18:45 +0100

    vertically centered the title text

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container bg-yellow-500 flex flex-row justify-between space-x-5 p-3">
-        <router-link to="/"><h1 class="font-sans text-base font-extrabold text-left pl-2">ER SHEKPE</h1> </router-link> 
+        <router-link to="/"><h1 class="font-sans text-base font-extrabold my-auto text-left pl-2">ER SHEKPE</h1> </router-link> 
         <my-search-bar class=""></my-search-bar>
         <my-cart-button :cartCount="cartCount" @cartClicked="goToCart"></my-cart-button>
     </div>
